6dbfb2f3efb14116af9b134a4f677f4ad8023784
linxiang <linxiang@linxiangdeMac-mini.local>
2017-05-04 17:44:53 +0800

    RACSequence

---
+++ LXReactiveCocoa_Demo/LXReactiveCocoa_Demo/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  LXReactiveCocoa_Demo
+//
+//  Created by linxiang on 2017/5/3.
+//  Copyright © 2017年 minxing. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LXReactiveCocoa_Demo
 What's RAC?   How to use RAC?
+
+RAC_GatherClass is a demo about RACSequence
+http://www.jianshu.com/p/f19f1591dfee
